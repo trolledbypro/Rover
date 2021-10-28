bb23aab9801fc92984c0a7771c78c26eadcdb893
Zachary Zalass <zachzalass@gmail.com>
2021-10-27 21:47:52 -0400

    Added declarations of pins, analog read

--- Transmitter/Transmitter_Simple/Transmitter_Simple.ino
+++ Transmitter/Transmitter_Simple/Transmitter_Simple.ino
@@ -13,11 +13,17 @@
 const int CE = 9;                   // Chip Enable
 const int CSN = 8;                  // Chip Select NOT
 
+const int button = 5;               // Button input pin
+const int pot = 26;                 // Potentiometer input pin
+int potValue = 0;                   // Store potentiometer value
+
+//**ADD SOME DATA TYPE STRUCT TO STORE VALUE OF POT AND BUTTON STATE**
+
 const byte address[6] = "00001";    // Address can be any 5 bit byte array
                                     // Must match receiver
 
 // Create Radio Object
-RF24 radio(9, 8);
+RF24 radio(CE, CSN);
 
 // Arduino Setup Section
 void setup() {
@@ -38,11 +44,15 @@ void setup() {
                                     // Defaults on pipe 0 (can have up to six pipes per receiver)
     radio.stopListening();          // Shuts off receiver function, this transceiver will only tramsit from now on
                                     // Put the above line in the loop section if we want to receive an acknowledgement packet 
+
+    pinMode(button, INPUT);         // Activate button input pin
 }
 
 // Arduino loop section
 void loop() {
-    const char text[] = "Hello World!";
+    potValue = analogRead(pot);     // Read value of potentiometer (0-1024? 0-5V)
+    //**ADD BUTTON STUFF**
+    //** CHANGE TO PUSH DATA TO TRANSMITTER**
     radio.write(&text, sizeof(text));   // Pass data by reference, also must pass size
     delay(1000);                    // 1 second delay
 }
