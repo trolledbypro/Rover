5d816d2f647821a12ba4757bec8ed1d98a89ecc9
Zachary Zalass <zachzalass@gmail.com>
2021-11-05 16:57:16 -0400

    More pins

--- Transmitter/Transmitter_Simple/Transmitter_Simple.ino
+++ Transmitter/Transmitter_Simple/Transmitter_Simple.ino
@@ -10,14 +10,28 @@
 #include <RF24.h>                   // Controls Radio
 
 // Define constants
-const int CE = 9;                   // Chip Enable
-const int CSN = 8;                  // Chip Select NOT
+#define CE 9                       // Chip Enable
+#define CSN 8                      // Chip Select NOT
+
+#define enableButton 20            // Button input pin
+#define directionButton 21         // Direction button input pin
+#define pot 26                      // Potentiometer input pin
+
+struct packet {
+    byte potValue = 0;              // Store potentiometer value
+    byte enableState= 0;            // Store button state for enable
+    byte swapDirection = 0;         // Store button state for direction change
+    // Byte due to PWM of sample DC motor
+};
 
 const byte address[6] = "00001";    // Address can be any 5 bit byte array
                                     // Must match receiver
 
 // Create Radio Object
-RF24 radio(9, 8);
+RF24 radio(CE, CSN);
+
+// Create Packet
+packet payload;
 
 // Arduino Setup Section
 void setup() {
@@ -28,7 +42,7 @@ void setup() {
     }
 
     // Code section below from Arduino Class Documentation
-    if (!radio.begin()) {
+    if (!radio.begin()) { 
         Serial.println("radio hardware not responding!");
         while (1) {}                //hold program in infinite loop to prevent subsequent errors
     }
@@ -38,11 +52,18 @@ void setup() {
                                     // Defaults on pipe 0 (can have up to six pipes per receiver)
     radio.stopListening();          // Shuts off receiver function, this transceiver will only tramsit from now on
                                     // Put the above line in the loop section if we want to receive an acknowledgement packet 
+
+    pinMode(enableButton, INPUT_PULLUP);            // Activate button input pin
+    pinMode(directionButton, INPUT_PULLUP);         // Activate button input pin
+
 }
 
 // Arduino loop section
 void loop() {
-    const char text[] = "Hello World!";
-    radio.write(&text, sizeof(text));   // Pass data by reference, also must pass size
-    delay(1000);                    // 1 second delay
+    payload.potValue = map(analogRead(pot), 0, 1023, 0, 255);// Record value of potentiometer (0-1024 mapped to byte size, 0-255)
+    payload.enableState = digitalRead(enableButton);         // Record button state
+    payload.swapDirection = digitalRead(directionButton);    // Record button state
+
+    radio.write(&payload, sizeof(payload));   // Pass data by reference, also must pass size
+    delay(1000);                              // 1 second delay
 }
